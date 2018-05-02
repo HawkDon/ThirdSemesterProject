38a75cbf00639a310a92f784468a2aa9ab7e5516
Mr. Awesome <2007.none@gmail.com>
2018-05-02 09:49:16 +0200

    Jeg har lavet dato og en submit knap til søgekriterier på flyrejser

---
+++ reactfrontend/src/Date.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default class DateRange extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment('2018-02-08'),
+      endDate: moment('2018-02-10')
+    }
+  }
+
+  handleChange = ({ startDate, endDate }) => {
+    startDate = startDate || this.state.startDate
+    endDate = endDate || this.state.endDate
+
+    if (startDate.isAfter(endDate)) {
+      endDate = startDate
+    }
+
+    this.setState({ startDate, endDate })
+  }
+  handleChangeStart = (startDate) => this.handleChange({ startDate })
+  handleChangeEnd = (endDate) => this.handleChange({ endDate })
+
+  render () {
+    return <div className="row">
+
+      <div className="column">
+      <label> Start Dato
+        <DatePicker
+          selected={this.state.startDate}
+          selectsStart
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeStart} />
+          </label>
+          <label> Slut Dato
+        <DatePicker
+          selected={this.state.endDate}
+          selectsEnd
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeEnd} />
+          </label>
+      </div>
+      <input type="submit" value="Submit" />
+    </div>
+  }
+}
--- reactfrontend/src/index.js
+++ reactfrontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import Container from './Container';
+import Date from './Date';
+ReactDOM.render(<Date />, document.getElementById('root'));
 registerServiceWorker();
