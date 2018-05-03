3fe159a5f5c4fe5fd2a918f8998ed86b33567aa1
Mr. Awesome <2007.none@gmail.com>
2018-05-03 10:15:06 +0100

    bilsøgning

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Lufthavnsspecs from './Lufthavnsspecs';
+import Bilsøgning from './Bilsøgning';
+import {
+  HashRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <Header />
+        <Switch>
+        <Route exact path="/" render={() => <Lufthavnsspecs />} />
+        <Route path="/Bilsøgning" render={() => <Bilsøgning/>} />
+        <Route component={NoMatch}/>
+        </Switch>
       </div>
+  </Router>
     );
   }
 }
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  </div>
+);
+const Header = () => (
+
+  <div>
+  <ul className="header">
+    <li><NavLink exact activeClassName="active" to="/">Flyrejse</NavLink></li>
+    <li><NavLink activeClassName="active" to="/Bilsøgning">Lej en bil</NavLink></li>
+  </ul>
+  </div>
+)
 
 export default App;
